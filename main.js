4acae667fdf154f387ad0ae1712554572ef5122a
st17109hr <49662888+st17109hr@users.noreply.github.com>
2020-10-23 10:55:02 +0900

    1タブ4スペースに変更

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 document.getElementById("view_time").innerHTML = getNow();
 function getNow(){//現在時刻の取得
-  var now = new Date();
-  var year = now.getFullYear();
-  var mon = now.getMonth()+1;
-  var day = now.getDate();
-  var hour = now.getHours();
-  var min = now.getMinutes();
-  var sec = now.getSeconds();
+    var now = new Date();
+    var year = now.getFullYear();
+    var mon = now.getMonth()+1;
+    var day = now.getDate();
+    var hour = now.getHours();
+    var min = now.getMinutes();
+    var sec = now.getSeconds();
 
-  var s = year + "年" + mon + "月" + day + "日" + hour + "時" + min + "分" + sec + "秒";
-  return s;
+    var s = year + "年" + mon + "月" + day + "日" + hour + "時" + min + "分" + sec + "秒";
+    return s;
 }
 
 function koshin(){
-  location.reload();
+    location.reload();
 }

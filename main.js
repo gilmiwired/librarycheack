1610803f72cf1588658e1c575751f2437160546b
st17109hr <49662888+st17109hr@users.noreply.github.com>
2020-10-26 13:51:10 +0900

    ページ更新の際の処理を追加

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 document.getElementById("view_time").innerHTML = getNow();
+var bgm;
+bgm = new Audio();
+bgm.src = "music/bgm.mp3"
+
 function getNow(){//現在時刻の取得
     var now = new Date();
     var year = now.getFullYear();
@@ -11,6 +15,16 @@ function getNow(){//現在時刻の取得
     var s = year + "年" + mon + "月" + day + "日" + hour + "時" + min + "分" + sec + "秒";
     return s;
 }
-function koshin(){
-	location.reload();
+
+function koshin(){//サイト更新ボタン
+    bgm.play(); //音楽再生
+    var result=confirm('更新するよ！')
+    if(result){ //サイト更新の確認
+        location.reload();
+    }
 }
+
+function open(){//ページを開いたときの処理。ここにカメラの更新と画像表示を記述
+    alert("ページが読み込まれました！");
+}
+window.onload = open();

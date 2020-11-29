233d1b2503daa9f015fd7aad413ddc1dac7d93c2
gilmi <65973449+gilmiwired@users.noreply.github.com>
2020-11-30 04:12:14 +0900

    画像用socket

--- socket/raspy/criant.py
+++ socket/raspy/criant.py
@@ -1,19 +1,17 @@
 import socket
 import pickle
 import cv2
+from PIL import Image
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # サーバを指定
     s.connect(('127.0.0.1', 50007))
     # サーバにメッセージを送る
-    s.sendall(b'hello mother fuckin')
-    # ネットワークのバッファサイズは1024。サーバからの文字列を取得する
-    data = s.recv(1024)
-    #
-    #print(repr(data))
-	im = cv2.imread(pickle.loads(data))
-	cv2.imshow('test', im)
-	cv2.waitKey()
-	cv2.destroyAllWindows()
-
-	print(im.shape)
+    s.sendall(b'hello mother fuckin beach')
+    # ネットワークのバッファサイズは1024。
+im = Image.open(pickle.loads(s.recv(1024)))
+im.save("logo_py.png", colors=255)
+cv2.imshow('test', im)
+cv2.waitKey()
+cv2.destroyAllWindows()
+print(im.shape)
--- socket/raspy/serv.py
+++ socket/raspy/serv.py
@@ -4,14 +4,14 @@ import cv2
 
 pic = pickle.dumps('taki.png')
 
-with socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # IPアドレスとポートを指定
     s.bind(('127.0.0.1', 50007))
-    # 1 接続
+    # 接続
     s.listen(1)
-    # connection するまで待つ
+    # connection待
     while True:
-        # 誰かがアクセスしてきたら、コネクションとアドレスを入れる
+        # アクセス時コネクションとアドレス交換
         conn, addr = s.accept()
         with conn:
             while True:
@@ -20,7 +20,6 @@ with socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 if not data:
                     break
                 print('data : {}, addr: {}'.format(data, addr))
-                # クライアントにデータを返す(b -> byte でないといけない)
                 # conn.sendall(b'Received: ' + data)
                 conn.sendall(b'Received:'+pic)
 print('end')

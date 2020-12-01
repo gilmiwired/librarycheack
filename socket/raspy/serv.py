35d6eb7c2d29a4a171b1598237a49fa699606765
gilmi <65973449+gilmiwired@users.noreply.github.com>
2020-12-02 06:31:51 +0900

    PY

--- socket/raspy/serv.py
+++ socket/raspy/serv.py
@@ -3,6 +3,12 @@ import cv2
 import io
 from PIL import Image
 
+IMAGE_QUALITY=30
+pic = cv2.imread("a.jpg")
+resized_img = cv2.resize(pic,(50,50))
+(status, encoded_img) = cv2.imencode('.jpg',pic, [int(cv2.IMWRITE_JPEG_QUALITY), IMAGE_QUALITY])
+print(type(encoded_img))
+print(encoded_img)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # IPアドレスとポートを指定
@@ -14,11 +20,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         # アクセス時コネクションとアドレス交換
         conn, addr = s.accept()
 
-        pic = Image.open("taki.png")
-        with io.BytesIO() as output:
-            pic.save(output,format="PNG")
-            picb = output.getvalue()#バイナリ取得
-
+        #pic = Image.open("taki.png")
+        #with io.BytesIO() as output:
+		#    pic.save(output,format="PNG")
+        #    picb = output.getvalue()#バイナリ取得
         with conn:
             while True:
                 # データを受け取る

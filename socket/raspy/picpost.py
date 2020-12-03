1c1e4eae3fdb782b72e47aba7a6127e2aed76a97
gilmi <65973449+gilmiwired@users.noreply.github.com>
2020-12-04 04:19:35 +0900

    ｇ

--- socket/raspy/picpost.py
+++ socket/raspy/picpost.py
@@ -2,5 +2,5 @@ import requests
 #こちら送信側→ローカルに置く
 with open("a.jpg", "rb") as f:#画像をopen(読み込みみたいな？)する
     #url='https://profile-counter.glitch.me/librarycheck/pic_save.php'#送り先
-	url='https://github.com/gilmiwired/librarycheck/img.jpg'
+	url='https://ibkosen-librarycondition.netlify.app/img/pic_save.php'
 	r = requests.post(url, f.read())#送信
